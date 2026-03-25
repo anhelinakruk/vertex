6d702ed24de092c5c8a25b7631c81561de360d4c
Daria Kozlovska <odriy.lovska@gmail.com>
2026-03-25 21:51:55 +0100

    Initial commit: Swift frontend

---
+++ vertex-frontend/vertex-frontend/vertex_frontendApp.swift
@@ -0,0 +1,17 @@
+//
+//  vertex_frontendApp.swift
+//  vertex-frontend
+//
+//  Created by Nikita Pidhornyi on 25/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct vertex_frontendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
